fe0350fc05f2e078ab4e4a0e680dab6e723e89f5
canhhuy116 <canhhuy1162002@gmail.com>
2022-07-16 14:35:45 +0700

    Initial Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import './App.scss';
+import Input from './components/Input/Input';
+import TodoList from './components/TodoList/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface AppProps {}
+
+interface AppState {}
+
+class App extends React.Component<AppProps, AppState> {
+  render() {
+    return (
+      <div className="App">
+        <h2>TO DO LIST</h2>
+        <Input />
+        <TodoList />
+      </div>
+    );
+  }
 }
 
 export default App;
